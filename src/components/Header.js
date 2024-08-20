66f14cfbff972ed3afbdb1a67b0ceceda7e345af
Cornelia Schulz <cornelia_schulz@trimble.com>
2024-08-20 16:25:06 +1200

    add link back to quiz list from individual quiz pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import sun from '../assets/images/icon-sun-light.svg';
 import sunDark from '../assets/images/icon-sun-dark.svg';
 import moon from '../assets/images/icon-moon-light.svg';
 import moonDark from '../assets/images/icon-moon-dark.svg';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header({isDark, onSwitch, title, icon}) {
@@ -9,10 +10,10 @@ function Header({isDark, onSwitch, title, icon}) {
   return (
     <div className="header">
       <header className="app-header">
-        <div className="app-header-title">
+        <Link to="/" className="app-header-title">
           {title && <img src={icon} className={title} alt="icon" />}
           {title && <p className={isDark ? "dark-header-title" : "header-title"}>{title}</p>}
-        </div>
+        </Link>
         <div className="app-header-toggle">
           <img src={isDark ? sun : sunDark} className="icon" alt="switch to light mode" />
           <label className="switch">
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -10,6 +10,7 @@ export default function Error() {
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
+        
       </p>
     </div>
   );
